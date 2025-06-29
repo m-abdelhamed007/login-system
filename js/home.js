efd00b80c92a1cd109ce6493057a50bf96840069
Mohamed Fawzy <Mohamed.Fawzy@EMEA.NEC.COM>
2025-06-30 01:34:23 +0300

    solved

--- js/home.js
+++ js/home.js
@@ -1,4 +1,10 @@
 var user =document.getElementById("userName");
  var userData = localStorage.getItem("currentUser");
+ var logoutBtn =document.getElementById("logoutBtn")
  var data = JSON.parse(userData);
  user.innerHTML = data.name;
+ logoutBtn.addEventListener('click',function(){
+     window.location.href = "./index.html";
+
+ });
+ 
